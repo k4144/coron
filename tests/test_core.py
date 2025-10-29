a5c74a6e8235db36686857c7de10c51bbaa6423c
k4144 <github.k4144@gmail.com>
2025-10-29 11:10:27 +0100

    *

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,30 +1,4 @@
-from coron.core import example_function, add_two, ExampleClass
-
-
-def test_method():
-    import os
-
-    print(os.getcwd())
-
-
-def test_methods():
-    try:
-        from src.coron.core import example_function, add_two
-        from src.coron.core import ExampleClass
-
-        def test_example_function():
-            assert example_function(1, 2) == 3
-
-        def test_add_two():
-            assert add_two(5) == 7
-
-        def test_example_class():
-            instance = ExampleClass()
-            assert instance.example_class_method() == "expected result"
-
-    except Exception as e:
-        print("cant import src.coron.core")
-        print(e)
+from coron.core import ExampleClass, add_two, example_function
 
 
 def test_example_function():
