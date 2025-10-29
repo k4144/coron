4287a6737550aceb1e6082e0332aefb45d755b94
k4144 <github.k4144@gmail.com>
2025-10-29 20:12:41 +0100

    *

--- src/coron/_version.py
+++ src/coron/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev26'
-__version_tuple__ = version_tuple = (0, 1, 'dev26')
+__version__ = version = '0.1.dev30'
+__version_tuple__ = version_tuple = (0, 1, 'dev30')
 
-__commit_id__ = commit_id = 'gf3c7ba3dd'
+__commit_id__ = commit_id = 'gc2bc3a3b9'
